0898f2177d26669cdc2560d8abbd859538501a74
smithy545 <smithy545@gmail.com>
2017-06-09 00:32:06 -0700

    tweaks

--- fourier.py
+++ fourier.py
@@ -3,45 +3,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = cv2.imread("cropped.jpg")
-points = np.load("perfect2.npy")
 
-sorted(points, key=lambda x: x[0])
+class Fourier:
+    def __init__(self):
+        self.points = points = np.load("perfect2.npy")
 
-xs = [p[0] for p in points]
-ys = [p[1] for p in points]
+        sorted(points, key=lambda x: x[0])
 
-amps = np.fft.fft(ys)
-freqs = np.fft.fftfreq(len(ys))
+        self.xs = np.array([p[0] for p in points])
+        self.ys = np.array([p[1] for p in points])
 
-# generate waves from fourier transform
-waves = []
-samples = np.arange(0, xs[-1], 0.1)
-for i, a in enumerate(amps):
-    wave = []
-    for t in samples: # for graphing
-        wave.append(a*np.cos(freqs[i]*t))
-    waves.append(wave)
+        self.amps = np.fft.fft(self.ys)
+        self.freqs = np.fft.fftfreq(self.ys.shape[-1])
 
+        # generate waves from fourier transform
+        self.waves = []
+        self.samples = np.arange(0, self.xs[-1], 0.1)
+        for i, a in enumerate(self.amps):
+            wave = []
+            for t in self.samples: # for graphing
+                wave.append(a*np.cos(self.freqs[i]*t))
+            self.waves.append(wave)
 
-# plot waves
-fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+        self.draw()
+        plt.show()
 
-# Real frequencies
-ax1.set_title("Real")
-for w in waves[1:]: #ignore the 0 frequency because its just a height adjustment
-    ax1.plot(samples, w)
+    def graph(self):
+        ax = plt.gca()
+        n = len(self.amps)
+        graph = [(x,y/n) for x,y in zip(self.freqs, self.amps)]
+        sorted(graph, key=lambda x: x[0])
+        x = [p[0] for p in graph]
+        y = [p[1] for p in graph]
+        ax.plot(x, y, 'k-')
 
-# Imaginary frequencies (they cancel out)
-ax2.set_title("Imaginary (cancels out)")
-for w in waves:
-    ax2.plot(samples, np.imag(w))
+    def draw(self):        
+        # plot waves
+        fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
 
-# Sister tattoo and data
-ax3.set_title("Tatoo")
-ax3.imshow(img, cmap="gray")
-ax3.scatter(xs, ys)
-ylim = ax3.get_ylim()
-ax3.set_ylim(ylim[1], ylim[0])
+        # Real frequencies
+        self.drawReal(ax1)
 
+        # Imaginary frequencies (they cancel out)
+        self.drawImag(ax2)
 
-plt.show()
+        # Sister tattoo and data
+        self.drawTattoo(ax3)
+
+        plt.show()
+
+    def drawReal(self, ax=None):
+        if ax == None:
+            ax = plt.gca()
+        ax.set_title("Real")
+        for w in self.waves[1:]: #ignore the 0 frequency because its just a height adjustment
+            ax.plot(self.samples, w)
+
+    def drawImag(self, ax=None):
+        if ax == None:
+            ax = plt.gca()
+        ax.set_title("Imaginary (cancels out)")
+        for w in self.waves:
+            ax.plot(self.samples, np.imag(w))
+
+    def drawTattoo(self, ax=None):
+        if ax == None:
+            ax = plt.gca()
+        ax.set_title("Tatoo")
+        ax.imshow(img, cmap="gray")
+        ax.scatter(self.xs, self.ys)
+        ylim = ax.get_ylim()
+        ax.set_ylim(ylim[1], ylim[0])
+
+f = Fourier()
